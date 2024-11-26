8b5c6474b627495f9ab0b0c74c04bbfcf0aa308f
Guilherme Montealvão <guilhermehmc06@gmail.com>
2024-11-26 16:59:40 -0400

    feat: exercicios

---
+++ MySQL/Trigger/Trigger - AFTER.sql
@@ -0,0 +1,24 @@
+create database Trigger_After_exercise;
+use Trigger_After_exercise;
+
+
+create table clients(
+id int auto_increment primary key,
+);
+
+create table sellers(
+id int auto_increment primary key
+);
+
+create table products(
+id int auto_increment primary key
+
+);
+
+create table sales(
+id int auto_increment primary key,
+);
+
+create table sales_audit(
+id int auto_increment primary key
+);
